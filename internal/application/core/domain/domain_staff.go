3607a159f97a3fac1b15b38616c9bf34d64c6cda
fullgukbap <2hanjunbum6@gmail.com>
2025-01-04 22:23:38 +0900

    fix: permission 삭제

--- internal/application/core/domain/domain_staff.go
+++ internal/application/core/domain/domain_staff.go
@@ -6,17 +6,10 @@ import (
 	"gorm.io/gorm"
 )
 
-type Permission string
-
-const (
-	Admin Permission = "ADMIN"
-)
-
 type Staff struct {
-	ID         int        `gorm:"primarykey"`
-	AccountID  string     `gorm:"varchar(255); not null"`
-	Password   string     `gorm:"varchar(255); not null"`
-	Permission Permission `gorm:"varchar(255); not null"`
+	ID        int    `gorm:"primarykey"`
+	AccountID string `gorm:"varchar(255); not null"`
+	Password  string `gorm:"varchar(255); not null"`
 
 	CreatedAt time.Time
 	UpdatedAt time.Time
