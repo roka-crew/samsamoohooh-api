0bcc686cfe03b4d2f659f7fdc74a2fc95e881da3
fullgukbap <2hanjunbum6@gmail.com>
2025-01-04 18:54:47 +0900

    feat: mysql 객체 구현

---
+++ internal/infra/persistence/mysql/mysql.go
@@ -0,0 +1,49 @@
+package mysql
+
+import (
+	"fmt"
+	"samsamoohooh-api/internal/infra/config"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+const (
+	format = "%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local"
+)
+
+type Mysql struct {
+	*gorm.DB
+}
+
+func NewMysql(
+	config *config.Config,
+) (*Mysql, error) {
+	dsn := fmt.Sprintf(format,
+		config.Mysql.User,
+		config.Mysql.Password,
+		config.Mysql.Host,
+		config.Mysql.Port,
+		config.Mysql.Database,
+	)
+
+	db, err := gorm.Open(
+		mysql.Open(dsn),
+		&gorm.Config{
+			TranslateError: true,
+		},
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to open gorm: %w", err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to unwarp sql.DB: %w", err)
+	}
+	sqlDB.SetMaxIdleConns(config.MysqlPool.SetMaxIdleConns)
+	sqlDB.SetMaxOpenConns(config.MysqlPool.SetMaxOpenConns)
+	sqlDB.SetConnMaxLifetime(config.MysqlPool.SetConnMaxLifetime.Duration())
+
+	return &Mysql{DB: db}, nil
+}
