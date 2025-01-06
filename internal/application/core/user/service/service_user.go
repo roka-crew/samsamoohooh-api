1739001a7e9a8e134542f990b203a4b02193515b
fullgukbap <2hanjunbum6@gmail.com>
2025-01-06 21:46:35 +0900

    feat: findUser, PatchUser service 구현

---
+++ internal/application/core/user/service/service_user.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"context"
+	"samsamoohooh-api/internal/application/core/user/store"
+	"samsamoohooh-api/internal/application/domain"
+	"samsamoohooh-api/internal/application/presenter"
+)
+
+type UserService struct {
+	userStore store.UserStore
+}
+
+func NewUserService(
+	userStore store.UserStore,
+) *UserService {
+	return &UserService{
+		userStore: userStore,
+	}
+}
+
+func (s *UserService) FindUser(ctx context.Context, req *presenter.FoundUserRequest) (*domain.User, error) {
+	foundUser, err := s.userStore.FindUser(ctx, req.ToParams())
+	if err != nil {
+		return nil, err
+	}
+
+	return foundUser, nil
+}
+
+func (s *UserService) PatchUser(ctx context.Context, req *presenter.PatchUserRequest) (*domain.User, error) {
+	patchedUser, err := s.userStore.PatchUser(ctx, req.ToParams())
+	if err != nil {
+		return nil, err
+	}
+
+	return patchedUser, nil
+}
